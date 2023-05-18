1d2036ed6dc03b39c0bac38488cf01c6c29aa4f4
Chris Short <chris@chrisshort.net>
2023-05-18 11:39:53 +0000

    days are optional and formatting is updated

--- certcheck.go
+++ certcheck.go
@@ -14,17 +14,22 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Please provide a URL (include https://) and the number of days")
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println("Please provide a URL (include https://) and an optional number of days")
 		os.Exit(1)
 	}
 
 	url := os.Args[1]
-	daysStr := os.Args[2]
-	days, err := parseDays(daysStr)
-	if err != nil {
-		fmt.Println("Invalid number of days:", err)
-		os.Exit(1)
+	var days int = -1 // Default value if days argument is not provided
+
+	if len(os.Args) == 3 {
+		daysStr := os.Args[2]
+		var err error
+		days, err = parseDays(daysStr)
+		if err != nil {
+			fmt.Println("Invalid number of days:", err)
+			os.Exit(1)
+		}
 	}
 
 	tr := &http.Transport{
@@ -43,25 +48,43 @@ func main() {
 	defer resp.Body.Close()
 
 	certs := resp.TLS.PeerCertificates
+	var validChain bool = true
+	for i := 0; i < len(certs)-1; i++ {
+		if certs[i].Issuer.CommonName != certs[i+1].Subject.CommonName {
+			validChain = false
+			break
+		}
+	}
+
 	for _, cert := range certs {
 		fmt.Printf("Subject: %s\n", cert.Subject.CommonName)
 		fmt.Printf("Issuer: %s\n", cert.Issuer.CommonName)
 		fmt.Printf("Valid from: %s\n", cert.NotBefore)
-		fmt.Printf("Valid until: %s ", cert.NotAfter)
+		fmt.Printf("Valid until: %s", cert.NotAfter)
 
-		daysLeft := int(cert.NotAfter.Sub(time.Now()).Hours()/24) + 1
-		if daysLeft <= days {
-			color.Red("(%d days left)", daysLeft)
-		} else {
-			fmt.Printf("(%d days left)", daysLeft)
+		if days != -1 {
+			daysLeft := int(time.Until(cert.NotAfter).Hours() / 24)
+			if daysLeft <= days {
+				color.Set(color.Bold, color.FgHiRed)
+				fmt.Printf(" (%d days left)", daysLeft)
+				color.Unset()
+			}
 		}
-		fmt.Println()
 
+		fmt.Println()
 		fmt.Printf("Serial number: %s\n", cert.SerialNumber.String())
 		fmt.Printf("DNS Names: %v\n", cert.DNSNames)
 		fmt.Printf("IP Addresses: %v\n", cert.IPAddresses)
 		fmt.Printf("Signature algorithm: %s\n", cert.SignatureAlgorithm.String())
 		fmt.Println("-----")
+	}
+
+	if validChain {
+		color.Set(color.Bold, color.FgHiGreen)
+		fmt.Println("Certificate chain is valid and in the correct order.")
+	} else {
+		color.Set(color.Bold, color.FgHiRed)
+		fmt.Println("Certificate chain is invalid or not in the correct order.")
 	}
 }
 
